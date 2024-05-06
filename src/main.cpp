3955dedea5760fb4459f74aedaa3df6826b19a59
zachwilliams16 <150628547+zachwilliams16@users.noreply.github.com>
2024-05-06 11:05:37 -0400

    created repository and backed up code

---
+++ src/main.cpp
@@ -0,0 +1,124 @@
+
+#include <Arduino.h>
+#include <SevSegShift.h>
+
+#define SHIFT_PIN_SHCP 6
+#define SHIFT_PIN_STCP 5
+#define SHIFT_PIN_DS 4
+#define BUTTON 7
+#define LED A1
+
+SevSegShift sevseg(SHIFT_PIN_DS, SHIFT_PIN_SHCP, SHIFT_PIN_STCP, 1, true);
+
+// Define global variables
+
+// Button variables
+int buttonState;
+int buttonUpState;
+
+unsigned long lastTimeIncremented = 0;
+unsigned long buttonDownSpeed = 25; // how fast you want the button to go when pressed down every x ms
+unsigned long lastButtonAttempt = 0;
+unsigned long lastSuccessfulButtonAttempt = 0;
+unsigned long debounceTime = 30;
+
+unsigned long timeNeededToTurnOffDebounce = 1000;
+bool debounceDisabled = false;
+
+unsigned long LargestHour = 1200;
+unsigned long LargestMin = 60;
+
+// hour and minutes are added together to get the time to display
+unsigned long hour = 1200;
+unsigned long minute = 0;
+unsigned long time = hour + minute;
+bool isAM = false; // if the LED should be on or not indicating if it is AM or PM
+
+unsigned long lastMinuteTick = millis();
+
+void checkMinute()
+{
+    if (minute > LargestMin - 1)
+    {
+        minute = 0;
+        hour += 100;
+    }
+    if (hour > LargestHour)
+    {
+        hour = 100;
+    }
+    time = hour + minute;
+}
+
+void timeAddMinute()
+{
+    minute++;
+    lastMinuteTick = millis();
+    checkMinute();
+}
+
+void buttonAddMinute()
+{
+    lastTimeIncremented = millis();
+    minute++;
+    checkMinute();
+}
+
+void buttonStuff()
+{
+    if (millis() - lastButtonAttempt > debounceTime) // if last time was at least x ago
+    {
+        debounceDisabled = false;
+        buttonAddMinute();
+        lastSuccessfulButtonAttempt = millis();
+    }
+    else if (millis() - lastSuccessfulButtonAttempt > timeNeededToTurnOffDebounce) // and if last successful press was more than x ago
+    {
+        debounceDisabled = true;
+    }
+    if (debounceDisabled) // if button is being held down
+    {
+        if (millis() - lastTimeIncremented > buttonDownSpeed)
+        {
+            buttonAddMinute();
+        }
+    }
+    lastButtonAttempt = millis();
+}
+
+void setup()
+{
+    byte numDigits = 4;
+    byte digitPins[] = {12, 11, 10, 9};            // These are the PINS of the ** Arduino **
+    byte segmentPins[] = {0, 1, 2, 3, 4, 5, 6, 7}; // these are the PINs of the ** Shift register **
+    bool resistorsOnSegments = false;              // 'false' means resistors are on digit pins
+    byte hardwareConfig = COMMON_CATHODE;          // See README.md for options
+    bool updateWithDelays = false;                 // Default 'false' is Recommended
+    bool leadingZeros = false;                     // Use 'true' if you'd like to keep the leading zeros
+    bool disableDecPoint = false;                  // Use 'true' if your decimal point doesn't exist or isn't connected
+    sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments, updateWithDelays, leadingZeros, disableDecPoint);
+    sevseg.setBrightness(50);
+    pinMode(BUTTON, INPUT_PULLUP);
+    Serial.begin(9600);
+    buttonUpState = digitalRead(BUTTON);
+}
+
+void loop()
+{
+    sevseg.setNumber(time);
+    sevseg.refreshDisplay(); // Must run repeatedly; don't use blocking code (ex: delay()) in the loop() function or this won't work right
+
+    if (digitalRead(BUTTON) != buttonUpState) // if button is pressed set buttonState to pressed and call buttonstuff
+    {
+        buttonState = digitalRead(BUTTON);
+        buttonStuff();
+    }
+    else // if button is not pressed sets button to not pressed
+    {
+        buttonState = buttonUpState;
+    }
+    if (millis() - lastMinuteTick > 60000)
+    {
+        timeAddMinute();
+    }
+}
